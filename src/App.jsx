3d688f08df2e8b008b55542d3a4af821547863c1
Ario09 <ario079k@gmail.com>
2026-09-12 22:06:34 -0300

    fix(lint): comentar en linea 6 import no usado en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import AppRoutes from './routes/routes'
-import useAutorizaciones from './hooks/useAutorizaciones'
+//import useAutorizaciones from './hooks/useAutorizaciones'
 
 function App() {
 return (
